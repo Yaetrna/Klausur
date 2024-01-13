077587d9f16ccbec7aa3da6b13d243075880ebbf
yaetrna <freyflorian727@protonmail.com>
2024-01-13 11:42:30 +0100

    Added Messung.java from "O Warum Arrays?"

---
+++ src/Messung.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Messung {
+    public static void main(String[] args) {
+
+        int arraySize = 5;
+        double arraySum = 0.0;
+        double[] array = new double[arraySize];
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < arraySize; i++) {
+            System.out.println("Messwert " + (i + 1) + ": ");
+            array[i] = scanner.nextDouble();
+            arraySum = arraySum + array[i];
+        }
+
+        System.out.println("Durchschnitt: " + arraySum / arraySize);
+
+        // array.length enthält den gleichen Wert wie arraySize!
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Messwert " + (i + 1) + ": " + array[i]);
+        }
+
+        /*
+        Neue Konstruktion - nicht relevant für die Klausur → entspricht demselben wie ein Absatz oben.
+
+        for (int i = 0; i < array.length; i++ {
+            System.out.println(array[i]);
+        }
+
+        IST DAS GLEICHE WIE
+
+        for (int value : array) {
+            System.out.println(value);
+        }
+         */
+
+        int iterator = 0;
+
+        for (double value : array) {
+            System.out.println("Messwert " + (iterator + 1) + ": " + value);
+            iterator++;
+        }
+    }
+}
